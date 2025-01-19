07d4ad22ad207a6363385e11f2497c372cede845
CAHbKA-Script <shura.kozel@list.ru>
2025-01-19 20:46:11 +0300

    feat: add full solution js-homework 4

--- main.js
+++ main.js
@@ -1,140 +1,140 @@
 //TASK 1
-console.log("Task 1:\n");
-let num1 = 1;
-while (num1 <=50) {
-    console.log(num1);
-    num1++;
-}
-console.log("Второй массив\n");
-
-for (let i = 35; i >=8; i--) {
-    console.log(i);
+console.log("\nTask 1:\n");
+let arr =  [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
 }
 
 //TASK 2
-document.write("Task 2:", "</br>");
-for (let i = 89; i >= 11; i--) {
-    document.write(i, "</br>");
+console.log("\nTask 2:\n");
+arr =  [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > -10 && arr[i] < -3) console.log(arr[i]);
 }
 
 //TASK 3
 console.log("\nTask 3:\n");
-let sum = 0;
-for (let i = 1; i <= 100; i++) {
-    sum += i;
+arr = [];
+let i = 23;
+while (i <= 57) {
+    arr.push(i);
+    i++;
 }
-console.log("Сумма чисел от 0 до 100 ровна " + sum);
+console.log("Цикл while: \n" + arr);
+
+arr = [];
+i = 23;
+for (;i <= 57; i++) {
+    arr.push(i);
+}
+console.log("Цикл for: \n" + arr);
 
 //TASK 4
-console.log("\nTask 4:");
-for (let i = 1; i <=5; i++) {
-    let sum = 0;
-    for (let j = 1; j <= i; j++) {
-        sum += j;
-    }
-    console.log("\nСумма чисел в числе " + i + " равна " + sum);
+console.log("\nTask 4:\n");
+arr = [];
+for (let i = 0; i < 100; i+=3) {
+    arr.push(String(i));
+}
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === "1" || arr[i][0] === "2" || arr[i][0] === "5") 
+        console.log(arr[i]);
 }
 
 //TASK 5
 console.log("\nTask 5:\n");
-console.log("Цикл while:\n");
-
-let key = 8;
-while (key <= 56) {
-    if (key % 2 === 0) console.log(key);
-    key++;
-}
-
-console.log("\nЦикл for:\n");
-for (let i = 8; i <= 56; i += 2) {
-    console.log(i);
+arr = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
+for (let i = 0; i < arr.length; i++) {
+    let tag = "<span";
+    if (arr[i] === "СБ" || arr[i] === "ВС") tag+= " class=\"weekend\"";
+    tag += (">" + arr[i] + "</span>");
+    document.write(tag);
 }
 
 //TASK 6
 console.log("\nTask 6:\n");
-
-for (let i = 2; i <= 10; i++) {
-    for(let j = 1; j <= 10; j++) {
-        console.log(i + "*" + j + "=" + i * j);
-    }
-    console.log("=======");
-}
+console.log(arr);
+console.log("Последний элмент: " + arr[arr.length - 1]);
 
 //TASK 7
 console.log("\nTask 7:\n");
-let res = n = 1000;
-let num = 0;
-
-while (res > 50) {
-    res /= 2;
-    num++;
+arr = [];
+while (true) {
+    let num = prompt("Ввдите число");
+    while (isNaN(num)) {
+        alert("Вы ввели не число! Введите число!");
+        num = prompt("Ввдите число");
+        if (!isNaN(num)) break;
+    }
+    if (num === "") break;
+    arr.push(num);
 }
-
-console.log("Полученное число: " + res + "\nКлдичесвто итераций: " + num);
+console.log(arr);
+arr.sort(function(a, b) {
+    return a - b;
+});
+console.log(arr);
 
 //TASK 8
-let sumTask8 = 0;
-let count = 0;
-while (true) {
-    let num = parseInt(prompt("Введите число"));
-    if (num === 0 || isNaN(num)) {
-        break;
-    }
-    sumTask8 += num;
-    count++;
-}
+console.log("\nTask 8:\n");
+arr =  [12, false, "Текст", 4, 2, -5, 0];
+console.log(arr.reverse());
 
-alert("Сумма введенных чисел: " + sumTask8 + "\nСреднее арифметическое: " + (sumTask8 / count));
+while(arr.length > 0) {
+    console.log(arr.shift());
+}
 
 //TASK 9
 console.log("\nTask 9:\n");
-let strNum = "4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57";
-let buffer = "";
-let min = 100;
-let max = 0;
-for (let i = 0; i < strNum.length; i++) {
-    if (strNum[i] !== " ") {
-        buffer += strNum[i];
-    }
-    else {
-        min = +buffer < min ? +buffer : min;
-        max = +buffer > max ? +buffer : max;
-        buffer = "";
+arr = [5, 9, 21, , , 9, 78, , , , 6];
+let count = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (typeof(arr[i]) === "undefined") {
+        count++;
     }
 }
-console.log("Минимальное число: " + min + "\nМаксимальное число: " + max);
+console.log("Количество нулевых элементов: " + count);
 
 //TASK 10
 console.log("\nTask 10:\n");
-let inputNumStr = prompt("Введите число");
+arr = [48, 9, 0, 4, 0, 21, 2, 1, 8, 84, 76, 8, 4, 13, 2, 0];
+let first = arr.indexOf(0);
+let last = arr.lastIndexOf(0);
+let sum = 0;
+if (first === -1 || last === -1 || (first === last)) {
+    console.log("Сумма ровна: 0\nНет двух нулей");
+}
+else {
+    for (let i = first; i < last; i++) {
+        sum += arr[i];
+    }
+    console.log("Сумма между двумя нулями:" + sum);
+}
 
+//TASK 11
+console.log("\nTask 11:\n");
+count = prompt("Введите высоту треугольника");
 while (true) {
-    if (inputNumStr === "" || isNaN(inputNumStr)) {
-        alert("Вы ввели не число");
-        inputNumStr = prompt("Введите число");
+    if (count === "" || isNaN(count)) {
+        alert("Пожалуйста, введите число");
+        count = prompt("Введите высоту треугольника");
     }
-    else {
-        break;
+    else break;
+}
+
+arr = [];
+for (let i = 1; i <= count; i++) {
+    arr.push([]);
+    for (let j = 0; j < count - i; j++) {
+        arr[i-1].push(" ");
+    }
+    for (let j = 0; j < i * 2 -1; j++) {
+        arr[i - 1].push("^");
     }
 }
-
-for (let i = 0; i < inputNumStr.length; i++) {
-    console.log(inputNumStr[i]);
+for (let i = 0; i < count; i++) {
+    console.log(arr[i].join(""));
 }
-
-let inputNum = Number(inputNumStr);
-let coeff = 10;
-let countTask10 = 0;
-let sumTask10 = 0;
-let reverse = "";
-
-
-while (inputNum > 0) {
-    sumTask10 += inputNum % coeff;
-    countTask10++;
-    reverse += inputNum % coeff;
-    inputNum = parseInt(inputNum / coeff);
-}
-
-alert("Сумма цифр: " + sumTask10 + "\nКоличество цифр: " +countTask10 + "\nОбратный порядок: " + reverse);
-    
+console.log(arr);
