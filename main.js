4baed6218674df14c87e09d36d2afc64f15db968
CAHbKA-Script <shura.kozel@list.ru>
2025-01-19 11:32:47 +0300

    feat: add full solution js-homework

--- main.js
+++ main.js
@@ -1,212 +1,140 @@
-alert("Task 1:");
-
-let name = prompt("Введите ваше имя");
-let age = prompt("Введите ваш возраст");
-let city = prompt("Введите ваш город");
-let phone = prompt("Введите ваш телефон");
-let email = prompt("Введите вашу почту");
-let company = prompt("Введите вашу компанию");
-
-alert("Меня зовут "+ name + ". Мне " + age + " лет. Я проживаю в городе " + city + " и работаю в компании " + company + ". Мои контактные данные: " + phone +", " + email + ".");
-
-alert("Task 2:");
-
-alert(name + " родился в " + (2025-age) + " году");
-
-let numStr = "234702";
-alert("Task 3:\n\"" + numStr +"\"");
-
-let leftPart = +numStr[0] + +numStr[1] + +numStr[2];
-let rightPart = +numStr[3] + +numStr[4] + +numStr[5];
-
-if (leftPart === rightPart) {
-    alert("Да! Равно.");
+//TASK 1
+console.log("Task 1:\n");
+let num1 = 1;
+while (num1 <=50) {
+    console.log(num1);
+    num1++;
 }
-else {
-    alert("Нет! Не равно.");
+console.log("Второй массив\n");
+
+for (let i = 35; i >=8; i--) {
+    console.log(i);
 }
 
-alert("Task 4:");
-
-let a = 1;
-if (a > 0) {
-    alert("a="+ a + "\nВерно! а больше 0");
-}
-else {
-    alert("a="+ a + "\nНе верно! а не больше 0");
+//TASK 2
+document.write("Task 2:", "</br>");
+for (let i = 89; i >= 11; i--) {
+    document.write(i, "</br>");
 }
 
-a = 0;
-if (a > 0) {
-    alert("a="+ a + "\nВерно! а больше 0");
+//TASK 3
+console.log("\nTask 3:\n");
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += i;
 }
-else {
-    alert("a="+ a + "\nНе верно! а не больше 0");
-}
+console.log("Сумма чисел от 0 до 100 ровна " + sum);
 
-a = -3;
-if (a > 0) {
-    alert("a="+ a + "\nВерно! а больше 0");
-}
-else {
-    alert("a="+ a + "\nНе верно! а не больше 0");
-}
-
-a = 10;
-let b = 2;
-alert("Task 5: \na=" + a +", b=" +b);
-let sum = a + b;
-
-alert("Сумма:" + sum +"\nРазность: " + (a - b) + "\nПроизведение: " + (a * b) + "\nЧастное: " + (a / b));
-if (sum > 1 ) {
-    alert("Квадрат суммы: " + sum**2);
+//TASK 4
+console.log("\nTask 4:");
+for (let i = 1; i <=5; i++) {
+    let sum = 0;
+    for (let j = 1; j <= i; j++) {
+        sum += j;
+    }
+    console.log("\nСумма чисел в числе " + i + " равна " + sum);
 }
 
-alert("Task 6: \n 2<a<11 или 6<=b<14");
-(2 < a && a < 11) || (6 <= b && b < 14) ? alert("Верно!") : alert("Не верно");
+//TASK 5
+console.log("\nTask 5:\n");
+console.log("Цикл while:\n");
 
-alert("Task 7:");
-let n = prompt("Введите число от 0 до 59");
-let quarter = String(n / 15);
+let key = 8;
+while (key <= 56) {
+    if (key % 2 === 0) console.log(key);
+    key++;
+}
 
-switch (quarter[0]) {
-    case "0": 
-        alert("Первая четверть");
+console.log("\nЦикл for:\n");
+for (let i = 8; i <= 56; i += 2) {
+    console.log(i);
+}
+
+//TASK 6
+console.log("\nTask 6:\n");
+
+for (let i = 2; i <= 10; i++) {
+    for(let j = 1; j <= 10; j++) {
+        console.log(i + "*" + j + "=" + i * j);
+    }
+    console.log("=======");
+}
+
+//TASK 7
+console.log("\nTask 7:\n");
+let res = n = 1000;
+let num = 0;
+
+while (res > 50) {
+    res /= 2;
+    num++;
+}
+
+console.log("Полученное число: " + res + "\nКлдичесвто итераций: " + num);
+
+//TASK 8
+let sumTask8 = 0;
+let count = 0;
+while (true) {
+    let num = parseInt(prompt("Введите число"));
+    if (num === 0 || isNaN(num)) {
         break;
-    case "1":
-        alert("Вторая часть");
-        break;
-    case "2": 
-        alert("Третья часть");
-        break;
-    case "3":
-        alert("Четвертая часть");
-        break;
-    default:
-        alert("Число не находится в диапазоне от 0 до 59");
+    }
+    sumTask8 += num;
+    count++;
 }
 
-alert("Task 8:");
-let day = +prompt("Введите количество дней для определения декады");
-let dayInMonth = day % 31;
-let decade = String((dayInMonth - 1) / 10);
+alert("Сумма введенных чисел: " + sumTask8 + "\nСреднее арифметическое: " + (sumTask8 / count));
 
-switch (decade[0]) {
-    case "0": 
-        alert("Первая декада");
+//TASK 9
+console.log("\nTask 9:\n");
+let strNum = "4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57";
+let buffer = "";
+let min = 100;
+let max = 0;
+for (let i = 0; i < strNum.length; i++) {
+    if (strNum[i] !== " ") {
+        buffer += strNum[i];
+    }
+    else {
+        min = +buffer < min ? +buffer : min;
+        max = +buffer > max ? +buffer : max;
+        buffer = "";
+    }
+}
+console.log("Минимальное число: " + min + "\nМаксимальное число: " + max);
+
+//TASK 10
+console.log("\nTask 10:\n");
+let inputNumStr = prompt("Введите число");
+
+while (true) {
+    if (inputNumStr === "" || isNaN(inputNumStr)) {
+        alert("Вы ввели не число");
+        inputNumStr = prompt("Введите число");
+    }
+    else {
         break;
-    case "1":
-        alert("Вторая декада");
-        break;
-    case "2": 
-        alert("Третья декада");
-        break;
-    default:
-        alert("Число не находится в диапазоне от 0 до 59");
-}
-
-alert("Task 9:");
-
-let days = Number(prompt("Введите количество дней")),
-    weeks = days / 7,
-    months = days / 31,
-    years = days / 365,
-    hours = days * 24;
-    minutes = days * 24 * 60;
-    seconds = days * 24 * 60 * 60;
-alert("Years: "+ years + "\nMonths: " + months + "\nDays: " + days + "\nHours: " + hours + "\nMinutes: " + minutes + "\nSeconds: " + seconds);
-
-if (weeks < 1) {
-    alert("Меньше недели");
-}
-else if (months < 1) {
-    alert("Меньше месяца");
-}
-else if (years < 1) {
-    alert("Меньше года");
-}
-else {
-    alert("Количество дней больше или равно 365");
+    }
 }
 
-alert("Task 10:");
-alert("Было введено: " + day + " дней");
-let month, timeOfYear;
-
-if (day <= 31) {
-    month = "Январь";
-}
-else if (31 < day && day <= 59) {
-    month = "Февраль";
-} 
-else if (59 < day && day <= 90) {
-    month = "Март";
-}
-else if (90 < day && day <= 120) {
-    month = "Апрель";
-}
-else if (120 < day && day < 151) {
-    month = "Май";
-}
-else if (151 < day && day <= 181) {
-    month = "Июнь";
-}
-else if (181 < day && day <= 212) {
-    month = "Июль";
-}
-else if (212 < day && day <= 243) {
-    month = "Август";
-}
-else if (243 < day && day <= 273) {
-    month = "Сентябрь";
-}
-else if (273 < day && day <= 304) {
-    month = "Октябрь";
-}
-else if (304 < day && day <= 334) {
-    month = "Ноябрь";
-}
-else if (334 < day && day <= 365) {
-    month = "Декабрь";
+for (let i = 0; i < inputNumStr.length; i++) {
+    console.log(inputNumStr[i]);
 }
 
-switch (month) {
-    case "Январь":
-        timeOfYear = "зима"
-        break;
-    case "Февраль":
-        timeOfYear = "зима"
-        break;
-    case "Март":
-        timeOfYear = "весна"
-        break; 
-    case "Апрель":
-        timeOfYear = "весна"
-        break;
-    case "Май":
-        timeOfYear = "весна"
-        break;
-    case "Июнь":
-        timeOfYear = "лето"
-        break; 
-    case "Июль":
-        timeOfYear = "лето"
-        break;
-    case "Август":
-        timeOfYear = "лето"
-        break;
-    case "Сентябрь":
-        timeOfYear = "осень"
-        break; 
-    case "Октябрь":
-        timeOfYear = "осень"
-        break;
-    case "Ноябрь":
-        timeOfYear = "осень"
-        break;
-    case "Январь":
-        timeOfYear = "зима"
-        break;    
+let inputNum = Number(inputNumStr);
+let coeff = 10;
+let countTask10 = 0;
+let sumTask10 = 0;
+let reverse = "";
+
+
+while (inputNum > 0) {
+    sumTask10 += inputNum % coeff;
+    countTask10++;
+    reverse += inputNum % coeff;
+    inputNum = parseInt(inputNum / coeff);
 }
 
-alert("Количество дней: " + day + "\nМесяц: " + month + "\nВремя года: " + timeOfYear);
+alert("Сумма цифр: " + sumTask10 + "\nКоличество цифр: " +countTask10 + "\nОбратный порядок: " + reverse);
+    
