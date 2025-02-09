c4e8f263869e626adf8a8462bd7642652c7e0be3
CAHbKA-Script <shura.kozel@list.ru>
2025-02-10 00:07:37 +0300

    feat: add full solution js-homework-7

--- main.js
+++ main.js
@@ -1,107 +1,134 @@
-//TASK 1
-console.log("\nTASK 1:\n");
-let str1 = "aaa@bbb@ccc";
-let regex = /@/g;
-console.log(str1.replace(regex, "!"));
-
-//TASK 2
-console.log("\nTASK 2:\n");
-let date = new Date();
-console.log(date);
-let checkDate = (date) => date < 10 ? "0" + date : date;
-let str2 = `${date.getFullYear()}-${checkDate(date.getMonth())}-${checkDate(date.getDate())}`
-console.log(str2);
-let regex2 = /(\d{4})-(\d{2})-(\d{2})/;
-console.log(str2.replace(regex2, '$3/$2/$1'));
-
-//TASK 3
-console.log("\nTASK 3:\n");
-let str3 = "Я учу javascript!";
-let word1 = "учу", word2 = "javascript";
-let res = str3.slice(str3.indexOf(word1), str3.indexOf(word1) + word1.length);
-console.log(res);
-res = str3.substring(str3.indexOf(word2), str3.indexOf(word2) + word2.length);
-console.log(res);
-res = str3.substr(str3.indexOf(word1), word1.length);
-console.log(res)
-
-//TASK 4
-console.log("\nTASK 4:\n");
-let arr = [4, 2, 5, 19, 13, 0, 10];
-
-res = Math.pow(arr.reduce((sum, el) => sum += Math.pow(el, 3), 0), 1/2);
-console.log(res);
-
-//TASK 5
-console.log("\nTASK 5:\n");
-let a = 3, b = 5;
-let c = Math.abs(a - b);
-console.log(`3 - 5 = ${a - b}; c = ${c}`);
-
-a = 6, b = 1;
-c = Math.abs(a - b);
-console.log(`3 - 5 = ${a - b}; c = ${c}`);
-
-//TASK 6
-console.log("\nTASK 6:\n");
-
-console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${checkDate(date.getDate())}.${checkDate(date.getMonth())}.${date.getFullYear()}`)
-
-//TASK 7
-console.log("\nTASK 7:\n");
-let str7 = "aa aba abba abbba abca abea";
-let regex7 = /ab+a/g;
-res = str7.match(regex7);
-console.log(...res);
-
-//TASK 8
-console.log("\nTASK 8:\n");
-
 function checkPhoneNumber (phone) {
     let regex = /\b\+?\d{3}\s?(\d{2}|\(\d{1,3}\))\s?\d{3}(?<sep>[\s-]?)\d{2}(\k<sep>)\d{2}\b/g;
-    console.log(false);
     return regex.test(phone);
 }
-
-while (true) {
-    let input = prompt ("Введите номер телефона\nпример: +375(29)1111111, 375 29 111 11 11, 37529 111-11-11");
-    if (checkPhoneNumber (input)) {
-        alert("Ваш номер телефона: " + input);
-        break;
-    }
-    else alert ("Что-то ввели не так " + input);
-}
-
-//TASK 9
-console.log("\nTASK 9:\n");
 
 function checkEmil (emil) {
     let regex = /\b[A-Za-z]\w{2,31}[A-Za-z]@[A-Za-z][A-Za-z\.-][A-Za-z]{1,9}(\.[A-Za-z]{2,11})\b/g;
     return regex.test(emil);
 }
 
-while (true) {
-    let input = prompt ("Введите электронную почту\nпример: mail@mail.by");
-    if (checkEmil (input)) {
-        alert("Ваша почта: " + input);
-        break;
-    }
-    else alert ("Что-то ввели не так " + input);
+function checkName (name) {
+    return /^[A-Z][a-z]{1,10}\s[A-Z][a-z]{1,10}(\s[A-Z][a-z]{1,10})?$/.test(name);
 }
 
-//TASK 10
-console.log("\nTASK 10:\n");
+function Contact () {
 
-function checkPath (path) {
-    let regex = /^(?<protocol>https?:\/\/)?(?<domain>[\w.\-]{5,40})(?<otherAddress>[\w.\-\/]{1,100}\/?)?(?<params>\?[\w\.&=\-]{1,100})?(?<hash>#[\w\.&=\-]{1,100})?$/g;
-    let res = regex.exec(path);
-    let returnArr = [];
-    for (let key in res.groups) {
-        returnArr.push(res.groups[key])
+    this.setName = (name) => {
+        if (!name) name = prompt ("Введите ФИО");
+        while (!checkName (name)) {
+            name = prompt ("Было введено некорректное ФИО, повторите ввод\nпример: Иван Иванов Иванович");
+            if (checkName (name)) {
+                alert("Вы ФИО: " + name);
+                break;
+            }
+            else alert ("Что-то ввели не так " + name);
+        }
+        return name;
     }
-    return returnArr;
+
+    this.getName = () => {
+        return this.name;
+    }
+
+    this.setAge = (age) => {
+        if (!age) age = prompt ("Введите возраст");
+        while (!age) {
+            if (+age >= 18 && +age <= 100) {
+                alert("Вы ввели возраст: " + age);
+                break;
+            }
+            else {
+                age = prompt ("Был введен некорректный возраст, повторите ввод (Возраст должен быть больше 18 и меньше 100)\n");
+                alert ("Что-то ввели не так " + age);
+            } 
+        }
+        return age;
+    }
+
+    this.setPhone = (phone) => {
+        if (!phone) phone = prompt ("Введите номер телефона");
+        while (!checkPhoneNumber (phone)) {
+            phone = prompt ("Был введен некорректный номер телефона, повторите ввод\nпример: +375(29)1111111, 375 29 111 11 11, 37529 111-11-11");
+            if (checkPhoneNumber (phone)) {
+                alert("Вы ввели номер телефона: " + phone);
+                break;
+            }
+            else alert ("Что-то ввели не так " + phone);
+        }
+        return phone;
+    }
+
+    this.setEmail = (email) => {
+        if (!email) email = prompt ("Введите адрес почты");
+        while (!checkEmil (email)) {
+            email = prompt ("Был введен некорректный адрес почты, повторите ввод\nпример: mail@mail.by");
+            if (checkPhoneNumber (email)) {
+                alert("Вы ввели адрес почты: " + email);
+                break;
+            }
+            else alert ("Что-то ввели не так " + email);
+        }
+        return email;
+    }
+
+    this.addDate = (name, age, phone, email) => {
+        this.name = this.setName (name);
+        this.age = this.setAge (age);
+        this.phone = this.setPhone (phone);
+        this.email = this.setEmail (email);
+    }
+
+    this.display = () => {
+        console.log(`Имя: ${this.name}. Возраст: ${this.age}. Телефон: ${this.phone}. Email: ${this.email}`);
+    }
+
 }
 
-let path = "https://tech.onliner.by/2018/04/26/smart-do-200/?utm_source=main_tile&utm_medium=smartdo200#zag3"
+function PhoneBook () {
+    this.contacts = [];
 
-console.log(checkPath(path));
+    this.addContact = (contact) => {
+        if (!contact) {
+            contact = new Contact();
+            contact.addContact();
+        }
+        this.contacts.push(contact);
+    }
+
+    this.displayContact = (name) => {
+        let res = this.contacts.filter((elem) => {
+            return elem.getName() === name;
+        });
+        if (res.length > 0) {
+            console.log(`\nПо ФИО ${name} найдено:`);
+            res.forEach((elem) => elem.display());
+        }
+        else {
+            console.log(`\nПо ФИО ${name} ничего не найдено:`);
+        }
+    }
+
+    this.displayAll = () => {
+        console.log("\nСписок контактов:");
+        this.contacts.forEach((elem) => elem.display());
+    }
+}
+
+const phoneBook = new PhoneBook ();
+let petr = new Contact ();
+petr.addDate("Ivanov Ivan Ivanovich", 19, "+375(29)123-45-67", "ivanov@mail.ru");
+phoneBook.addContact(petr);
+let misha = new Contact ();
+misha.addDate("Zybenko Mikhail Petrovich", 45, "+375(33)777-77-77", "zybenko777p@mail.ru");
+phoneBook.addContact(misha);
+let vasiliy = new Contact ();
+vasiliy.addDate("Vasko Vasiliy Ivanovich", 88, "+375 44 873-23-23", "vasiliy@mail.ru");
+phoneBook.addContact(vasiliy);
+let alex = new Contact ();
+alex.addDate("Kozel Alexandr Igorevich", 22, "+375(33)1212121", "alex_mail@mail.com");
+phoneBook.addContact(alex);
+phoneBook.displayContact("Kozel Alexandr Igorevich");
+phoneBook.displayContact("asd asd asd");
+
+phoneBook.displayAll();
